786ab98af595f4d0ab90cf028528b0f9625cd91e
shishir <shishir4913120@gmail.com>
2018-05-24 21:39:15 +0545

    db

--- hrproject/src/main/webapp/resources/db/hr_form_verification.sql
+++ hrproject/src/main/webapp/resources/db/hr_form_verification.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2018 at 03:35 AM
+-- Generation Time: May 24, 2018 at 05:53 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -23,6 +23,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `general_settings`
+--
+
+CREATE TABLE IF NOT EXISTS `general_settings` (
+  `general_settings_id` bigint(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `general_settings`
+--
+
+INSERT INTO `general_settings` (`general_settings_id`, `name`, `description`) VALUES
+(1, 'image_upload_location', 'localhost:8080/hr_images'),
+(2, 'image_download_location', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usertbl`
 --
 
@@ -35,21 +55,52 @@ CREATE TABLE IF NOT EXISTS `usertbl` (
   `email` varchar(50) NOT NULL,
   `status` varchar(100) NOT NULL DEFAULT '1',
   `name` varchar(100) NOT NULL,
-  `res2` varchar(100) DEFAULT NULL,
+  `advertiseno` varchar(25) DEFAULT NULL,
   `res3` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usertbl`
 --
 
-INSERT INTO `usertbl` (`userid`, `username`, `password`, `citizenshipno`, `phonenumber`, `email`, `status`, `name`, `res2`, `res3`) VALUES
-(6, 'admin', 'admin', '123456', '1234567891', 'admin@admin.com', '1', 'Admin Admin', NULL, NULL),
-(7, 'sunil', 'sunil', '123456', '9848023649', 'sunil@gmail.com', '1', 'sunil karki', NULL, NULL);
+INSERT INTO `usertbl` (`userid`, `username`, `password`, `citizenshipno`, `phonenumber`, `email`, `status`, `name`, `advertiseno`, `res3`) VALUES
+(6, 'admin', 'admin', '123456', '1234567891', 'admin@admin.com', '1', 'Admin Admin', '2/2075-2076', NULL),
+(9, 'shishirkarki', 'shishir', '123456', '9845811254', 'shishir@gmail.com', '1', 'Shishir', '1/2075-2076', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vacancytbl`
+--
+
+CREATE TABLE IF NOT EXISTS `vacancytbl` (
+  `id` int(11) DEFAULT NULL,
+  `advertiseno` varchar(25) DEFAULT NULL,
+  `posten` varchar(30) DEFAULT NULL,
+  `postnp` varchar(30) DEFAULT NULL,
+  `serviceen` varchar(30) DEFAULT NULL,
+  `servicenp` varchar(30) DEFAULT NULL,
+  `groupen` varchar(30) DEFAULT NULL,
+  `groupnp` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vacancytbl`
+--
+
+INSERT INTO `vacancytbl` (`id`, `advertiseno`, `posten`, `postnp`, `serviceen`, `servicenp`, `groupen`, `groupnp`) VALUES
+(1, '1/2075-2076', 'Section Officer', 'acdag', 'General Service', 'adjk', 'Service', 'abck'),
+(2, '2/2075-2076', 'Section Officer1', 'acdag', 'General Service1', 'adjk', 'Service1', 'abck');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `general_settings`
+--
+ALTER TABLE `general_settings`
+  ADD PRIMARY KEY (`general_settings_id`);
 
 --
 -- Indexes for table `usertbl`
@@ -62,10 +113,15 @@ ALTER TABLE `usertbl`
 --
 
 --
+-- AUTO_INCREMENT for table `general_settings`
+--
+ALTER TABLE `general_settings`
+  MODIFY `general_settings_id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `usertbl`
 --
 ALTER TABLE `usertbl`
-  MODIFY `userid` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `userid` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
